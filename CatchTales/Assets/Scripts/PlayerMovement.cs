0bc97bd66544e4233603527c5428928e30aa0da6
최동우 <fdf5548@naver.com>
2023-09-06 15:24:33 +0900

    Feat : Update files about player
    Former-commit-id: d1386a57bf7b83c5671112d1852d2058b60398a7

--- CatchTales/Assets/Scripts/PlayerMovement.cs
+++ CatchTales/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a921bd5dfd60639d2e862f4edfac99607c607deddbb819f5648b63976b4c240
-size 1662
+oid sha256:4f685587993fb323734f07b73d545c1e3fc380d297d7ff959ff49699f82539bc
+size 1724
