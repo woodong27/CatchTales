ba42479b9938f9e4c78dd9d8bb117e21ab24cce2
최동우 <fdf5548@naver.com>
2023-09-07 11:23:00 +0900

    Feat : Update camera view
    Former-commit-id: b936b103680440218e45b41d0ae2a37b94e41013

---
+++ CatchTales/Assets/Scripts/PlayerMovementSolo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2517bbb6529c64dc2a23fe30fa78a55036ccd939aab30f88daa191e5e2856bcc
+size 1680
---
+++ CatchTales/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e96d67a61980173b66354f7370b04f46d8ee3c16ea15d2f314aeaf56e61b6806
+size 1062
