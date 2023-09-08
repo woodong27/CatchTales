0ab01915009fc4627e8d8bc7c4f170607208b9f7
박소윤 <barcardi26@gmail.com>
2023-09-08 17:27:43 +0900

    Update README.md
    Former-commit-id: e236695999614b4e18e0802921ee9737f993c5bc

--- CatchTales/Assets/Scripts/LobbyManager.cs
+++ CatchTales/Assets/Scripts/LobbyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:391b7de7808026df5c4ec254996b152eb278fdd356408a79645dfdd0e780c600
-size 3387
+oid sha256:0ed1a46d597b4e123306547d82e4e75e9bc102d35a28eda51d3b528ab1d117e5
+size 3309
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbc5fe2310bbd9abb8013356bdc5712d7c101593d57ae5d0dcbbef9b9725d88a
-size 752
+oid sha256:416c068214e370150f44e03a35db5e04d2dfedae174e9e2a9585ec0bb753b0c2
+size 871
