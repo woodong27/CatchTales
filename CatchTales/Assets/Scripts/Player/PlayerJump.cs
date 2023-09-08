ebb8f340cd44fb9eafe159b5cba81e98306f8319
최동우 <fdf5548@naver.com>
2023-09-08 17:17:02 +0900

    Feat : Create file for player
    Former-commit-id: 29de17ea3c808904e27c35dfa9da3c6b278d1188

---
+++ CatchTales/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a9b737e0ef4e36fab7c977eff82efa66ca794f9776bd13ffcc493e514577fe3
+size 925
---
+++ CatchTales/Assets/Scripts/Player/ThirdPersonController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7546d96b1c90e79c5da590e985f5edeacdeef9f138bc857b42f6e4ea1031e534
+size 2637
