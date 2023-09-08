6d7142ff029bb80f4a5170fc5cedce936ab652e6
최동우 <fdf5548@naver.com>
2023-09-08 16:18:15 +0900

    Feat : Update files about moving
    Former-commit-id: 45622703b0fceeaa4b63e6aa97d639e42753ea22

--- CatchTales/Assets/Scripts/ThirdPersonCamera.cs
+++ CatchTales/Assets/Scripts/ThirdPersonCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a98e2443fb2b3e210e108c973e3f6a6b70e9113ac21ed28814c8ec87d8f1f767
-size 2509
+oid sha256:b3d8004ecc9e1e900d4d88c628ef598ef10c95ac59ae5c057a8adb23a2601c3d
+size 2595
