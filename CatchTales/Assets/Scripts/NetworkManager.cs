15ef5465c6ee5e0f506f41bf302698ca3cc815eb
KWON SEONGEUN <kss4037@gmail.com>
2023-09-08 17:08:41 +0900

    feat: Lobby Maked
    Former-commit-id: 7ca6c967a2bdc8d67a591615607757a79584f910

--- CatchTales/Assets/Scripts/NetworkManager.cs
+++ CatchTales/Assets/Scripts/NetworkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54cea93fffe248040a533258d42cb923da53caf44aebce24b61f5212bf15adbd
-size 6652
+oid sha256:c36da1940ac59a82ab7fca74f7b25f66d739bef9e48b26ba20a3f763314b32e8
+size 9489
