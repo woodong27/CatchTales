922498e4313ba7e817591e4ace54d56c66864d94
최동우 <fdf5548@naver.com>
2023-09-08 11:58:18 +0900

    Feat : Update files about third person camera view
    Former-commit-id: 5ca5e68107c6e671d7e09d4b61c1f1e828504dbc

--- CatchTales/Assets/Scripts/PlayerMovementSolo.cs
+++ CatchTales/Assets/Scripts/PlayerMovementSolo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2517bbb6529c64dc2a23fe30fa78a55036ccd939aab30f88daa191e5e2856bcc
-size 1680
+oid sha256:30e58824bbbff79f3b7dc45f42757b288d5e2ce854ae511d68ff91896684b725
+size 1666
