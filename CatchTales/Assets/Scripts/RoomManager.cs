a583b36518c5498c13305b159b2621932ff12076
KWON SEONGEUN <kss4037@gmail.com>
2023-09-08 11:58:06 +0900

    feat/Lobby-Remakeed
    Former-commit-id: 5f4034a6f8f17d5a8e75ef654e73f98df19c3ecc

--- CatchTales/Assets/Scripts/RoomManager.cs
+++ CatchTales/Assets/Scripts/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8398edad1b78aa80f9a07d90840af6d58207456e7564fcd82556da1bf3a11c18
-size 3295
+oid sha256:ef52d6628017d33c4e21d9642b4ff903680bd616f2fbaf1218455a14a168bad4
+size 3204
