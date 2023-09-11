e30590d0a274766cf10e4fb79d9818f6179f791a
최동우 <fdf5548@naver.com>
2023-09-11 15:25:56 +0900

    Feat : 빨간모자 상호작용 추가
    Former-commit-id: 8ef4959ac5138588466a1cc732fc9b2a76cd4d82

--- CatchTales/Assets/Scripts/Player/PlayerInteraction.cs
+++ CatchTales/Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb4f5644f23c74f702b780763b98c2193027d87286dbcc97b1a1bf3d40372f6
-size 1444
+oid sha256:728c46e7bfc189d735100badbee30e3dfec9cbb87d6be341c913734e92c51e49
+size 1489
