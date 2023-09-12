359e998ed0e04620c759edb063fe772030680f83
최동우 <fdf5548@naver.com>
2023-09-12 16:18:09 +0900

    Chore : Update red hood
    Former-commit-id: 46b65ee7a96928a816d040eeb0798cf6780b8d59

--- CatchTales/Assets/Scripts/Player/PlayerJump.cs
+++ CatchTales/Assets/Scripts/Player/PlayerJump.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2071aec2900d617412bbbcc7b9fa13228935d66addcf4fc0719d66f1dc2991f8
+oid sha256:38e6384b8250e72f7f1417bd9e4370c3477c5e4b6093c02c5b42f2603de2d5c7
 size 964
